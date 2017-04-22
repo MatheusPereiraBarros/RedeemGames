4e1ef1d55f73bbc0810ac0b6ffba56f7942bbf36
MatheusPereiraBarros <matheuspereirabarros@gmail.com>
2017-04-22 18:29:45 -0300

    Mais uma modificação no static files

--- RedeemGames/settings.py
+++ RedeemGames/settings.py
@@ -117,6 +117,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = "/var/www/example.com/static/"
