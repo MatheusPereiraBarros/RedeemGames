649701041ad5f7955ea03ca3185a4a76f53e8a68
MatheusPereiraBarros <matheuspereirabarros@gmail.com>
2017-04-24 21:50:41 -0300

    Modificações nos arquivos de template e de view

--- RedeemGamesWEB/urls.py
+++ RedeemGamesWEB/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post),
+    url(r'^$', views.jogo),
 ]
 
 
--- RedeemGamesWEB/views.py
+++ RedeemGamesWEB/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Jogo
+from .models import PostGameSite
 
-def post_list(request):
+def jogo(request):
 	jogo = Jogo.objects.all()
-	return render(request, 'RedeemGamesWEB/post_list.html', {'usuario': jogo})
+	return render(request, 'RedeemGamesWEB/post_list.html', {'game': jogo})
 
+def post(request):
+	post = PostGameSite.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'RedeemGamesWEB/post_list.html', {'postagem': post})
 # Create your views here.
