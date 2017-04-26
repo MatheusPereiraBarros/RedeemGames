8ae739c9d3697ac6ed2887ea9eeb3658d123238e
MatheusPereiraBarros <matheuspereirabarros@gmail.com>
2017-04-26 00:46:33 -0300

    Integração inicial do template com o banco de dados

--- RedeemGamesWEB/views.py
+++ RedeemGamesWEB/views.py
@@ -5,9 +5,10 @@ from .models import PostGameSite
 
 def jogo(request):
 	jogo = Jogo.objects.all()
-	return render(request, 'RedeemGamesWEB/post_list.html', {'game': jogo})
+	return render(request, 'RedeemGamesWEB/post_list.html', {'jogo': jogo})
 
-def post(request):
+'''def post_list(request):
 	post = PostGameSite.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-	return render(request, 'RedeemGamesWEB/post_list.html', {'postagem': post})
+	jogos = Jogo.objects.all()
+	return render(request, 'RedeemGamesWEB/post_list.html', {'jogos': jogos})'''
 # Create your views here.
